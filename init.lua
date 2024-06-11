6db191d8a60062e49b75e06d49e7fec12295ef3a
squirrelbaffler <theodore.d.lincoln@gmail.com>
2024-06-10 22:16:17 -0400

    Some colorscheme stuff

--- init.lua
+++ init.lua
@@ -700,21 +700,34 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+  {
+    'rebelot/kanagawa.nvim',
+    priority = 1000,
     init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      vim.cmd.colorscheme 'kanagawa'
+    end,
+  },
+  {
+    'folke/tokyonight.nvim',
+  },
+  {
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_italic = true
+    end,
+  },
+  {
+    'sainnhe/everforest',
+    lazy = false,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.everforest_enable_italic = true
+      vim.g.everforest_background = 'hard'
+      -- vim.cmd.colorscheme 'everforest'
     end,
   },
 
